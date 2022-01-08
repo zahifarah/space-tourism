3e05532a1179a99c8e441434d69b0196f07b391a
Zahi Farah <zahi.farah@protonmail.com>
2022-01-08 11:04:58 +0200

    Create colors section of design system page

--- src/index.js
+++ src/index.js
@@ -5,16 +5,58 @@ import "./CSS/index.css"
 
 const Website = () => {
   return (
-    <div class="container bg-dark">
+    <div class="container bg-dark text-white">
       <h1>Design system</h1>
 
-      <div class="bg-light text-dark">light blue bg, with dark text</div>
+      <section id="colors">
+        <h2>
+          <span>01</span> colors
+        </h2>
 
-      <div class="bg-white text-dark">white bg, with dark text</div>
+        <div class="flex">
+          <div style={{ flexGrow: "1" }}>
+            <div
+              class="bg-dark text-white"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}>
+              #0B0D17
+            </div>
+            <p>
+              <span class="text-accent">RGB</span> 11, 13, 23
+            </p>
+            <p>
+              <span class="text-accent">HSL</span> 230°, 35%, 7%
+            </p>
+          </div>
 
-      <div class="example-component bg-light text-white">
-        light blue bg with a 20% alpha and white text
-      </div>
+          <div style={{ flexGrow: "1" }}>
+            <div
+              class="bg-accent text-dark"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}>
+              #D0D6F9
+            </div>
+            <p>
+              <span class="text-accent">RGB</span> 208, 214, 249
+            </p>
+            <p>
+              <span class="text-accent">HSL</span> 231°, 77%, 90%
+            </p>
+          </div>
+
+          <div style={{ flexGrow: "1" }}>
+            <div
+              class="bg-white text-dark"
+              style={{ padding: "3rem 1rem 1rem", border: "1px solid white" }}>
+              #FFFFFF
+            </div>
+            <p>
+              <span class="text-accent">RGB</span> 255, 255, 255
+            </p>
+            <p>
+              <span class="text-accent">HSL</span> 0°, 0%, 100%
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
